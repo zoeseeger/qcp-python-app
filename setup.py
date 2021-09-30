90058f46aec43721de22cc5b70b009f1729d03a2
Zoe Seeger <zoe.seeger@monash.edu>
2021-09-30 11:29:49 +1000

    update from monarch

--- setup.py
+++ setup.py
@@ -1,52 +1,52 @@
-from setuptools import setup, find_packages
+import sys
+from distutils.core import setup
 
-# SETUP METADATA, USES setuptools.setup and .find_packages
+
+# CHECK PYTHON IS AT LEAST PYTHON VERSION 3
+if sys.hexversion < 0x3000000:
+    msg = "Python version %s is unsupported, >= 3.0.0 is needed"
+    print(msg % (".".join(map(str, sys.version_info[:3]))))
+    exit(1)
+
+# REWRITE SHEBANG LINE
+path_to_python3 = sys.executable
+lines = open("qcp/qcp", "r+").read()
+with open('qcp/qcp', 'w+') as f:
+    f.write(lines.replace('/bin/env python3', path_to_python3))
+
+# CHECK NUMPY IS INSTALLED
+try:
+        import numpy
+except ImportError:
+        sys.exit("Numpy is a dependency of qcp and this python version does \n" +
+                 "not seem to have numpy installed. Your options are:\n"        +
+                 "1. 'pip3 install numpy' with sudo access\n"                   +
+                 "2. 'pip3 install numpy' in a virtualenv of python\n"          +
+                 "3. Comment out this section and procede with some functionality loss\n")
+
+# SETUP METADATA
 setup(
     # APPLICATION DETAILS
     name             = "qcp",
-    version          = "2.0",
+    version          = "2.0.0",
     author           = "Zoe L. Seeger",
     author_email     = "zoe.seeger@monash.edu",
-    url              = "https://github.com/zoeseeger/qcp-python-module",
+    url              = 'https://gitlab.erc.monash.edu/zlsee3/qcp',
     license          = "LICENSE.txt",
     description      = "GAMESS, PSI4 & GAUSSIAN I/O processor",
     long_description = open("README.txt").read(),
 
-    # FOR SETUPTOOLS
-    packages=find_packages(),
+    # MY PACKAGES TO INSTALL
+    packages=["qcp"],
 
-    # FILES TO INCLUDE : IF SET TO TRUE WILL READ FROM MANIFEST.in
-    include_package_data=True,
+    # install_requires is a function of setuptools
+    # not distutils - as it is more likely that
+    # numpy will be installed than setuptools
+    # requiring the user to have setuptools may
+    # be more unhelpful than helpful
+    #install_requires=['numpy', 'python>=3'],
 
-    # SYSTEM AND PIP METADATA
-    classifiers=[
-        # only compatible with Python 3
-        "Programming Language :: Python :: 3",
-        # licensed under GNU GENERAL PUBLIC LICENSE
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        # OS-independent
-        "Operating System :: OS Independent",
-        # TAGS
-        "Topic :: Scientific/Engineering :: Chemistry",
-        "Natural Language :: English",
-        "Development Status :: 5 - Production/Stable"
-        ],
-
-    # REQUIRES PYTHON VERSION
-    python_requires='>=3.0',
-
-    # DEPENDENCIES TO INSTALL IF NOT AVAILABLE
-    install_requires=[
-        "numpy",
-        ],
-
-    # CREATE EXCECUTABLE
-    entry_points={
-        "console_scripts" : [
-            "qcp = qcp.__init__:main"
-            ],
-        "gui_scripts" : [
-            ]
-        },
-
+    # CREATE bin WITH QCP IN
+    scripts=['qcp/qcp'],
 )
+
